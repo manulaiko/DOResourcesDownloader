61ea26a388f7d2f692c25b63c9123fdb242aa4c4
Manulaiko <manulaiko@gmail.com>
2016-12-02 00:06:18 +0100

    Created Settings class

---
+++ src/com/manulaiko/dord/launcher/Settings.java
@@ -0,0 +1,63 @@
+package com.manulaiko.dord.launcher;
+
+import java.io.File;
+
+/**
+ * Settings class.
+ *
+ * Contains the configuration for the application.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+public class Settings
+{
+    /**
+     * Whether we're running on debug mode or not.
+     */
+    public static boolean debug = false;
+
+    /**
+     * Whether we're downloading all files or not.
+     */
+    public static boolean downloadAll = false;
+
+    /**
+     * Whether we're just downloading XML files or not.
+     */
+    public static boolean downloadXML = false;
+
+    /**
+     * Whether we're just downloading SWF files or not.
+     */
+    public static boolean downloadSWF = false;
+
+    /**
+     * Whether we're just downloading 2D files or not.
+     */
+    public static boolean download2D = false;
+
+    /**
+     * Whether we're just downloading 3D files or not.
+     */
+    public static boolean download3D = false;
+
+    /**
+     * Whether we're just downloading image files or not.
+     */
+    public static boolean downloadImages = false;
+
+    /**
+     * Where to download the resources.
+     */
+    public static File path = new File("./");
+
+    /**
+     * Proxy host.
+     */
+    public static String proxyHost = "";
+
+    /**
+     * Proxy port.
+     */
+    public static int proxyPort = 0;
+}
